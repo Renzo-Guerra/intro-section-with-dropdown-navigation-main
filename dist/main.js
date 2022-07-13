505c79f370ed79eeb1f4c29bf8175383e857ebd7
Renzo-Guerra <renzoguerralatt@gmail.com>
2022-07-13 20:32:58 -0300

    Restructured the main HTML, the hero img was hiden to work with the responsive navbar

--- dist/main.js
+++ dist/main.js
@@ -2,6 +2,174 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/scss/main.scss":
+/*!****************************!*\
+  !*** ./src/scss/main.scss ***!
+  \****************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/sourceMaps.js */ "./node_modules/css-loader/dist/runtime/sourceMaps.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "/* Box sizing rules */\n*,\n*::before,\n*::after {\n  box-sizing: border-box;\n  margin: 0;\n  padding: 0;\n}\n\n/* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */\nul[role=list],\nol[role=list] {\n  list-style: none;\n}\n\n/* Set core root defaults */\nhtml:focus-within {\n  scroll-behavior: smooth;\n}\n\n/* Set core body defaults */\nbody {\n  min-height: 100vh;\n  text-rendering: optimizeSpeed;\n  line-height: 1.5;\n  font-size: 100%;\n}\n\n/* A elements that don't have a class get default styles */\na:not([class]) {\n  text-decoration-skip-ink: auto;\n}\n\n/* Make images easier to work with */\nimg,\npicture,\nsvg {\n  max-width: 100%;\n  display: block;\n}\n\n/* Inherit fonts for inputs and buttons */\ninput,\nbutton,\ntextarea,\nselect {\n  font: inherit;\n}\n\n/* Remove all animations, transitions and smooth scroll for people that prefer not to see them */\n@media (prefers-reduced-motion: reduce) {\n  html:focus-within {\n    scroll-behavior: auto;\n  }\n  *,\n*::before,\n*::after {\n    animation-duration: 0.01ms !important;\n    animation-iteration-count: 1 !important;\n    transition-duration: 0.01ms !important;\n    scroll-behavior: auto !important;\n  }\n}\n/*--------------*/\n/*--Typography--*/\n/*--------------*/\n/*--------------*/\n/*----Colors----*/\n/*--------------*/\n/*\n  The designs were created to the following widths:\n\n  - Mobile: 375px\n  - Desktop: 1440px\n*/\nfooter {\n  font-size: 11px;\n  text-align: center;\n}\nfooter a {\n  color: hsl(228deg, 45%, 44%);\n}", "",{"version":3,"sources":["webpack://./src/scss/base/_reset.scss","webpack://./src/scss/main.scss","webpack://./src/scss/base/_typography.scss","webpack://./src/scss/base/_variables.scss","webpack://./src/scss/layout/_footer.scss"],"names":[],"mappings":"AAEA,qBAAA;AACA;;;EAGE,sBAAA;EACA,SAAA;EACA,UAAA;ACDF;;ADIA,2GAAA;AACA;;EAEE,gBAAA;ACDF;;ADIA,2BAAA;AACA;EACE,uBAAA;ACDF;;ADIA,2BAAA;AACA;EACE,iBAAA;EACA,6BAAA;EACA,gBAAA;EACA,eAAA;ACDF;;ADIA,0DAAA;AACA;EACE,8BAAA;ACDF;;ADIA,oCAAA;AACA;;;EAGE,eAAA;EACA,cAAA;ACDF;;ADIA,yCAAA;AACA;;;;EAIE,aAAA;ACDF;;ADIA,gGAAA;AACA;EACE;IACC,qBAAA;ECDD;EDIA;;;IAGE,qCAAA;IACA,uCAAA;IACA,sCAAA;IACA,gCAAA;ECFF;AACF;AC9DA,iBAAA;AACA,iBAAA;AACA,iBAAA;ACFA,iBAAA;AACA,iBAAA;AACA,iBAAA;AAEA;;;;;CAAA;ACFA;EACE,eFEU;EEDV,kBAAA;AH0EF;AGzEE;EACE,4BDUa;AFiEjB","sourcesContent":["// https://piccalil.li/blog/a-modern-css-reset\r\n\r\n/* Box sizing rules */\r\n*,\r\n*::before,\r\n*::after {\r\n  box-sizing: border-box;\r\n  margin: 0;\r\n  padding: 0;\r\n}\r\n\r\n/* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */\r\nul[role='list'],\r\nol[role='list'] {\r\n  list-style: none;\r\n}\r\n\r\n/* Set core root defaults */\r\nhtml:focus-within {\r\n  scroll-behavior: smooth;\r\n}\r\n\r\n/* Set core body defaults */\r\nbody {\r\n  min-height: 100vh;\r\n  text-rendering: optimizeSpeed;\r\n  line-height: 1.5;\r\n  font-size: 100%;\r\n}\r\n\r\n/* A elements that don't have a class get default styles */\r\na:not([class]) {\r\n  text-decoration-skip-ink: auto;\r\n}\r\n\r\n/* Make images easier to work with */\r\nimg,\r\npicture,\r\nsvg {\r\n  max-width: 100%;\r\n  display: block;\r\n}\r\n\r\n/* Inherit fonts for inputs and buttons */\r\ninput,\r\nbutton,\r\ntextarea,\r\nselect {\r\n  font: inherit;\r\n}\r\n\r\n/* Remove all animations, transitions and smooth scroll for people that prefer not to see them */\r\n@media (prefers-reduced-motion: reduce) {\r\n  html:focus-within {\r\n   scroll-behavior: auto;\r\n  }\r\n  \r\n  *,\r\n  *::before,\r\n  *::after {\r\n    animation-duration: 0.01ms !important;\r\n    animation-iteration-count: 1 !important;\r\n    transition-duration: 0.01ms !important;\r\n    scroll-behavior: auto !important;\r\n  }\r\n}","/* Box sizing rules */\n*,\n*::before,\n*::after {\n  box-sizing: border-box;\n  margin: 0;\n  padding: 0;\n}\n\n/* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */\nul[role=list],\nol[role=list] {\n  list-style: none;\n}\n\n/* Set core root defaults */\nhtml:focus-within {\n  scroll-behavior: smooth;\n}\n\n/* Set core body defaults */\nbody {\n  min-height: 100vh;\n  text-rendering: optimizeSpeed;\n  line-height: 1.5;\n  font-size: 100%;\n}\n\n/* A elements that don't have a class get default styles */\na:not([class]) {\n  text-decoration-skip-ink: auto;\n}\n\n/* Make images easier to work with */\nimg,\npicture,\nsvg {\n  max-width: 100%;\n  display: block;\n}\n\n/* Inherit fonts for inputs and buttons */\ninput,\nbutton,\ntextarea,\nselect {\n  font: inherit;\n}\n\n/* Remove all animations, transitions and smooth scroll for people that prefer not to see them */\n@media (prefers-reduced-motion: reduce) {\n  html:focus-within {\n    scroll-behavior: auto;\n  }\n  *,\n*::before,\n*::after {\n    animation-duration: 0.01ms !important;\n    animation-iteration-count: 1 !important;\n    transition-duration: 0.01ms !important;\n    scroll-behavior: auto !important;\n  }\n}\n/*--------------*/\n/*--Typography--*/\n/*--------------*/\n/*--------------*/\n/*----Colors----*/\n/*--------------*/\n/*\n  The designs were created to the following widths:\n\n  - Mobile: 375px\n  - Desktop: 1440px\n*/\nfooter {\n  font-size: 11px;\n  text-align: center;\n}\nfooter a {\n  color: hsl(228deg, 45%, 44%);\n}","/*--------------*/\r\n/*--Typography--*/\r\n/*--------------*/\r\n\r\n$fs-paragraph: 18px;\r\n$fs-footer: 11px;\r\n$ff: 'Kumbh Sans', sans-serif;\r\n$fw-500: 500;\r\n$fw-700: 700;","/*--------------*/\r\n/*----Colors----*/\r\n/*--------------*/\r\n\r\n/*\r\n  The designs were created to the following widths:\r\n\r\n  - Mobile: 375px\r\n  - Desktop: 1440px\r\n*/\r\n\r\n//Neutral\r\n\r\n$color-almost-white: hsl(0, 0%, 98%);\r\n$color-medium-gray: hsl(0, 0%, 41%);\r\n$color-almost-black: hsl(0, 0%, 8%);\r\n$color-footer-a: hsl(228, 45%, 44%);","@use '../base/' as *;\r\n\r\nfooter{\r\n  font-size: $fs-footer;\r\n  text-align: center;\r\n  a{\r\n    color: $color-footer-a;\r\n  }\r\n}"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/runtime/api.js":
+/*!*****************************************************!*\
+  !*** ./node_modules/css-loader/dist/runtime/api.js ***!
+  \*****************************************************/
+/***/ ((module) => {
+
+
+
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Tobias Koppers @sokra
+*/
+module.exports = function (cssWithMappingToString) {
+  var list = []; // return the list of modules as css string
+
+  list.toString = function toString() {
+    return this.map(function (item) {
+      var content = "";
+      var needLayer = typeof item[5] !== "undefined";
+
+      if (item[4]) {
+        content += "@supports (".concat(item[4], ") {");
+      }
+
+      if (item[2]) {
+        content += "@media ".concat(item[2], " {");
+      }
+
+      if (needLayer) {
+        content += "@layer".concat(item[5].length > 0 ? " ".concat(item[5]) : "", " {");
+      }
+
+      content += cssWithMappingToString(item);
+
+      if (needLayer) {
+        content += "}";
+      }
+
+      if (item[2]) {
+        content += "}";
+      }
+
+      if (item[4]) {
+        content += "}";
+      }
+
+      return content;
+    }).join("");
+  }; // import a list of modules into the list
+
+
+  list.i = function i(modules, media, dedupe, supports, layer) {
+    if (typeof modules === "string") {
+      modules = [[null, modules, undefined]];
+    }
+
+    var alreadyImportedModules = {};
+
+    if (dedupe) {
+      for (var k = 0; k < this.length; k++) {
+        var id = this[k][0];
+
+        if (id != null) {
+          alreadyImportedModules[id] = true;
+        }
+      }
+    }
+
+    for (var _k = 0; _k < modules.length; _k++) {
+      var item = [].concat(modules[_k]);
+
+      if (dedupe && alreadyImportedModules[item[0]]) {
+        continue;
+      }
+
+      if (typeof layer !== "undefined") {
+        if (typeof item[5] === "undefined") {
+          item[5] = layer;
+        } else {
+          item[1] = "@layer".concat(item[5].length > 0 ? " ".concat(item[5]) : "", " {").concat(item[1], "}");
+          item[5] = layer;
+        }
+      }
+
+      if (media) {
+        if (!item[2]) {
+          item[2] = media;
+        } else {
+          item[1] = "@media ".concat(item[2], " {").concat(item[1], "}");
+          item[2] = media;
+        }
+      }
+
+      if (supports) {
+        if (!item[4]) {
+          item[4] = "".concat(supports);
+        } else {
+          item[1] = "@supports (".concat(item[4], ") {").concat(item[1], "}");
+          item[4] = supports;
+        }
+      }
+
+      list.push(item);
+    }
+  };
+
+  return list;
+};
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/runtime/sourceMaps.js":
+/*!************************************************************!*\
+  !*** ./node_modules/css-loader/dist/runtime/sourceMaps.js ***!
+  \************************************************************/
+/***/ ((module) => {
+
+
+
+module.exports = function (item) {
+  var content = item[1];
+  var cssMapping = item[3];
+
+  if (!cssMapping) {
+    return content;
+  }
+
+  if (typeof btoa === "function") {
+    var base64 = btoa(unescape(encodeURIComponent(JSON.stringify(cssMapping))));
+    var data = "sourceMappingURL=data:application/json;charset=utf-8;base64,".concat(base64);
+    var sourceMapping = "/*# ".concat(data, " */");
+    var sourceURLs = cssMapping.sources.map(function (source) {
+      return "/*# sourceURL=".concat(cssMapping.sourceRoot || "").concat(source, " */");
+    });
+    return [content].concat(sourceURLs).concat([sourceMapping]).join("\n");
+  }
+
+  return [content].join("\n");
+};
+
+/***/ }),
+
 /***/ "./src/index.html":
 /*!************************!*\
   !*** ./src/index.html ***!
@@ -39,7 +207,7 @@ var ___HTML_LOADER_REPLACEMENT_7___ = _node_modules_html_loader_dist_runtime_get
 var ___HTML_LOADER_REPLACEMENT_8___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_8___);
 var ___HTML_LOADER_REPLACEMENT_9___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_9___);
 var ___HTML_LOADER_REPLACEMENT_10___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_10___);
-var code = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n  <meta charset=\"UTF-8\">\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n  <link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"" + ___HTML_LOADER_REPLACEMENT_0___ + "\">\n  <title>Frontend Mentor | Intro section with dropdown navigation</title>\n  <!-- google fonts -->\n  <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n  <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n  <link href=\"https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap\" rel=\"stylesheet\">\n</head>\n<body>\n  <header>\n    <a href=\"./index.html\"><img src=\"" + ___HTML_LOADER_REPLACEMENT_1___ + "\" alt=\"Website's logo\"></a>\n    <nav>\n      <div>\n        <li>Features <i><img src=\"" + ___HTML_LOADER_REPLACEMENT_2___ + "\" alt=\"Icon arrow down\"></i>\n          <ul>\n            <li><i><img src=\"" + ___HTML_LOADER_REPLACEMENT_3___ + "\" alt=\"Icon planing\"></i>Todo List</li>\n            <li><i><img src=\"" + ___HTML_LOADER_REPLACEMENT_4___ + "\" alt=\"Icon calendar\"></i>Calendar</li>\n            <li><i><img src=\"" + ___HTML_LOADER_REPLACEMENT_5___ + "\" alt=\"Icon reminders\"></i>Reminders</li>\n            <li><i><img src=\"" + ___HTML_LOADER_REPLACEMENT_3___ + "\" alt=\"Icon planing\"></i>Planing</li>\n          </ul>\n        </li>\n        <li>Company\n          <ul>\n            <li>History</li>\n            <li>Our Team</li>\n            <li>Blog</li>\n          </ul>\n        </li>\n        <li>Careers</li>\n        <li>About</li>\n      </div>\n      <div>\n        <button>Register</button>\n        <button>Login</button>\n      </div>\n      <button>Close Button</button>\n    </nav>\n    <button>Hamburguer button</button>\n  </header>\n  <section>\n    <div>\n      <h1>Make remote work</h1>\n      <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>\n      <button>Learn more</button>\n    </div>\n    <figure>\n      <img src=\"" + ___HTML_LOADER_REPLACEMENT_6___ + "\" alt=\"\">\n    </figure>\n    <figure>\n      <img src=\"" + ___HTML_LOADER_REPLACEMENT_7___ + "\" alt=\"Databiz company logo\">\n      <img src=\"" + ___HTML_LOADER_REPLACEMENT_8___ + "\" alt=\"Audiophile company logo\">\n      <img src=\"" + ___HTML_LOADER_REPLACEMENT_9___ + "\" alt=\"Meet company logo\">\n      <img src=\"" + ___HTML_LOADER_REPLACEMENT_10___ + "\" alt=\"Maker company logo\">\n    </figure>\n  </section>\n  <footer>\n    <p>Challenge by <a href=\"https://www.frontendmentor.io?ref=challenge\" target=\"_blank\">Frontend Mentor</a>.</p>\n    <p>Coded by <a href=\"https://gist.github.com/Renzo-Guerra\">Renzo Guerra</a>.</p>\n  </footer>\n</body>\n</html>";
+var code = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n  <meta charset=\"UTF-8\">\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n  <link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"" + ___HTML_LOADER_REPLACEMENT_0___ + "\">\n  <title>Frontend Mentor | Intro section with dropdown navigation</title>\n  <!-- google fonts -->\n  <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n  <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n  <link href=\"https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap\" rel=\"stylesheet\">\n</head>\n<body>\n  <header>\n    <a href=\"./index.html\"><img src=\"" + ___HTML_LOADER_REPLACEMENT_1___ + "\" alt=\"Website's logo\"></a>\n    <nav>\n      <div>\n        <li>Features <i><img src=\"" + ___HTML_LOADER_REPLACEMENT_2___ + "\" alt=\"Icon arrow down\"></i>\n          <ul>\n            <li><i><img src=\"" + ___HTML_LOADER_REPLACEMENT_3___ + "\" alt=\"Icon planing\"></i>Todo List</li>\n            <li><i><img src=\"" + ___HTML_LOADER_REPLACEMENT_4___ + "\" alt=\"Icon calendar\"></i>Calendar</li>\n            <li><i><img src=\"" + ___HTML_LOADER_REPLACEMENT_5___ + "\" alt=\"Icon reminders\"></i>Reminders</li>\n            <li><i><img src=\"" + ___HTML_LOADER_REPLACEMENT_3___ + "\" alt=\"Icon planing\"></i>Planing</li>\n          </ul>\n        </li>\n        <li>Company\n          <ul>\n            <li>History</li>\n            <li>Our Team</li>\n            <li>Blog</li>\n          </ul>\n        </li>\n        <li>Careers</li>\n        <li>About</li>\n      </div>\n      <div>\n        <button>Register</button>\n        <button>Login</button>\n      </div>\n      <button>Close Button</button>\n    </nav>\n    <button>Hamburguer button</button>\n  </header>\n  <section>\n    <figure style=\"display: none;\">\n      <img src=\"" + ___HTML_LOADER_REPLACEMENT_6___ + "\" alt=\"\">\n    </figure>\n    <div>\n      <main>\n        <h1>Make remote work</h1>\n        <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>\n        <button>Learn more</button>\n      </main>\n      <!-- Displayed to \"none\" in order to design the responsive navbar -->\n      <figure>\n        <img src=\"" + ___HTML_LOADER_REPLACEMENT_7___ + "\" alt=\"Databiz company logo\">\n        <img src=\"" + ___HTML_LOADER_REPLACEMENT_8___ + "\" alt=\"Audiophile company logo\">\n        <img src=\"" + ___HTML_LOADER_REPLACEMENT_9___ + "\" alt=\"Meet company logo\">\n        <img src=\"" + ___HTML_LOADER_REPLACEMENT_10___ + "\" alt=\"Maker company logo\">\n      </figure>\n    </div>\n    </section>\n  <footer>\n    <p>Challenge by <a href=\"https://www.frontendmentor.io?ref=challenge\" target=\"_blank\">Frontend Mentor</a>.</p>\n    <p>Coded by <a href=\"https://gist.github.com/Renzo-Guerra\">Renzo Guerra</a>.</p>\n  </footer>\n</body>\n</html>";
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (code);
 
@@ -204,7 +372,7 @@ module.exports = __webpack_require__.p + "1c174ccea84ec59ff880.svg";
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
+/******/ 			id: moduleId,
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
 /******/ 		};
@@ -327,6 +495,8 @@ var __webpack_exports__ = {};
   \************************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _index_html__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../index.html */ "./src/index.html");
+/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../scss/main.scss */ "./src/scss/main.scss");
+
 
 })();
 
